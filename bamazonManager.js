ad1ada415d70e9301c1e042abdf2f306eb881317
Denton Kerr <dentonkerr@Dentons-MacBook-Pro.local>
2018-02-22 16:21:25 -0500

    adding functions to manager

--- bamazonManager.js
+++ bamazonManager.js
@@ -29,23 +29,23 @@ function runSearch() {
                 "Add new product"
             ]
         })
-        .then(function(answer) {
+        .then(function (answer) {
             switch (answer.action) {
                 case "View products for sale":
-                viewProducts();
-                break;
+                    viewProducts();
+                    break;
 
                 case "View low inventory":
-                lowInventory();
-                break;
+                    lowInventory();
+                    break;
 
                 case "Add to inventory":
-                addInventory();
-                break;
+                    addInventory();
+                    break;
 
                 case "Add new product":
-                newProduct();
-                break;
+                    newProduct();
+                    break;
             }
         })
 }
@@ -54,8 +54,69 @@ function viewProducts() {
     connection.query("SELECT * FROM products", function (err, res) {
         for (var i = 0; i < res.length; i++) {
             console.log("\n" + res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity + "\n");
+            console.log("-----------------------------------");
         }
-        console.log("-----------------------------------");
+        runSearch();
     })
+}
+
+function lowInventory() {
+    connection.query("SELECT * FROM products WHERE stock_quantity < 5", function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log("\n" + res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity + "\n");
+            console.log("-----------------------------------");
+        }
+        runSearch();
+    })
+}
+
+function addInventory() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What item would you like to add inventory to?",
+                name: "add",
+                validate: function (value) {
+                    if (isNaN(value) === false && value < 11) {
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            {
+                type: "input",
+                message: "How many would you like to add?",
+                name: "quantity",
+                validate: function (value) {
+                    if (isNaN(value) === false && value < 11) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ])
+        .then(function (answer) {
+            var chosenItem;
+            connection.query("SELECT * FROM products WHERE ?", { id: answer.add },
+                function (err, res) {
+                    for (var i = 0; i < res.length; i++) {
+                        console.log("\n" + res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity + "\n")
+                        chosenItem = res[i];
+                    }
+                    connection.query("UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                stock_quantity: parseInt(chosenItem.stock_quantity) + parseInt( answer.quantity)
+                            },
+                            {
+                                id: chosenItem.id
+                            }
+                        ])
+                    console.log("\n" + "Quantity of " + answer.quantity + " added to ID: " + chosenItem.id + " Name: " + chosenItem.product_name + "\n");
+                    runSearch();
+                }
+            )
+        })
 }
 
